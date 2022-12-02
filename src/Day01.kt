bc5ca87e89d714a44d12d64d37a0b7d8f386182a
Ivan Vasilev <vianomore@gmail.com>
2022-12-02 22:24:25 +0100

    Day 1 solution (merged)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun solve(input: List<String>): Int {
+    fun part1(input: List<String>): Int {
         return input
             .joinToString("\n")
             .split("\n\n")
@@ -12,7 +12,22 @@ fun main() {
             .max()
     }
 
+    fun part2(input: List<String>): Int {
+        return input
+            .joinToString("\n")
+            .split("\n\n")
+            .map{ chunk ->
+                chunk
+                    .split("\n")
+                    .map{ it.toInt() }
+                    .sum()
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
+
     val input = readInput("Day01")
-    val solution = solve(input)
-    println(solution)
+    println(part1(input))
+    println(part2(input))
 }
