6ff40234647a8069d583c0d1eebef007ee5e5337
Ivan Vasilev <vianomore@gmail.com>
2022-12-02 23:34:56 +0100

    Day 2 solution (part2), refactor part1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,19 +1,51 @@
 fun main() {
-   fun solve(input: List<String>): Int {
+   fun solve1(input: List<String>): Int {
        return input.fold(0) { acc, pair ->
            val (he, me) = pair.split(" ")
            val heInt = he.first().code
            val meInt = me.first().code
            val bonus = meInt - 87
-           when (heInt - meInt) {
-               -23 -> acc + 3 + bonus
-               -21, -24 -> acc + 6 + bonus
-               else -> acc + bonus
+           acc + bonus + when (heInt - meInt) {
+               -23 -> 3
+               -21, -24 -> 6
+               else -> 0
            }
        }
    }
 
+    fun solve2(input: List<String>): Int {
+        return input.fold(0) { acc, pair ->
+            val (he, me) = pair.split(" ")
+            val heInt = he.first().code
+            val meInt = me.first().code
+            val bonus = (meInt - 88) * 3
+            acc + bonus + when (heInt - 64) {
+                1 -> {
+                    when (meInt - 87) {
+                        1 -> 3
+                        2 -> 1
+                        else -> 2
+                    }
+                }
+                2 -> {
+                    when (meInt - 87) {
+                        1 -> 1
+                        2 -> 2
+                        else -> 3
+                    }
+                }
+                else -> {
+                    when (meInt - 87) {
+                        1 -> 2
+                        2 -> 3
+                        else -> 1
+                    }
+                }
+            }
+        }
+    }
+
     val input = readInput("Day02")
-    val solution = solve(input)
-    println(solution)
+    println(solve1(input))
+    println(solve2(input))
 }
