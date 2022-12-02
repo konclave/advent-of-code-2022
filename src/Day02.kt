83ae62b557c7100c72f32429b6efd257ed148898
Ivan Vasilev <vianomore@gmail.com>
2022-12-02 23:15:41 +0100

    Day 2 solution (part1)

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,20 +1,19 @@
 fun main() {
    fun solve(input: List<String>): Int {
-        return input
-        	.joinToString("\n")
-	        .split("\n\n")
-	        .map{ chunk ->
-                chunk
-                    .split("\n")
-                    .map{ it.toInt() }
-                    .sum()
-            }
-	        .sortedDescending()
-	        .take(3)
-	        .sum()
-    }
+       return input.fold(0) { acc, pair ->
+           val (he, me) = pair.split(" ")
+           val heInt = he.first().code
+           val meInt = me.first().code
+           val bonus = meInt - 87
+           when (heInt - meInt) {
+               -23 -> acc + 3 + bonus
+               -21, -24 -> acc + 6 + bonus
+               else -> acc + bonus
+           }
+       }
+   }
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     val solution = solve(input)
     println(solution)
 }
