a159d52e6d33f5d8f55a81cfc4b529f4db735795
Ivan Vasilev <vianomore@gmail.com>
2022-12-02 22:18:30 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun solve(input: List<String>): Int {
+        return input
+            .joinToString("\n")
+            .split("\n\n")
+            .map{ chunk ->
+                chunk
+                    .split("\n")
+                    .map{ it.toInt() }
+                    .sum()
+            }
+            .max()
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val solution = solve(input)
+    println(solution)
 }
